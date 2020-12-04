6f73e254a9db3c3bbae2af4b380a2ecaa5676ccb
mehdi hadeli <mehdi.hadeli@outlook.com>
2020-12-04 23:59:34 +0330

    .

--- scripts/dotnet-pack-micro-bootstrap.sh
+++ scripts/dotnet-pack-micro-bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo Executing after success scripts on branch ${GITHUB_REF#refs/heads/}
+echo Executing after success scripts on branch $Branch_Name
 echo Triggering MyGet package build
-echo 1.0.$CI_RUN_ID
+echo 1.0.$Github_ID
 
 cd src/MicroBootstrap
-dotnet pack /p:PackageVersion=1.0.$CI_RUN_ID  --no-restore -o .
+dotnet pack /p:PackageVersion=1.0.$Github_ID  --no-restore -o .
 
-echo Uploading MicroBootstrap package to MyGet using branch ${GITHUB_REF#refs/heads/}
+echo Uploading MicroBootstrap package to MyGet using branch $Branch_Name
 
-case "${GITHUB_REF#refs/heads/}" in
+case "$Branch_Name" in
   "master")
     dotnet nuget push *.nupkg -s https://www.myget.org/F/micro-bootstrap/api/v3/index.json -k $MYGET_API_KEY 
     ;;
@@ -17,9 +17,9 @@ case "${GITHUB_REF#refs/heads/}" in
     ;;    
 esac
 
-echo Uploading MicroBootstrap package to Nuget using branch ${GITHUB_REF#refs/heads/}
+echo Uploading MicroBootstrap package to Nuget using branch $Branch_Name
 
-case "${GITHUB_REF#refs/heads/}" in
+case "$Branch_Name" in
   "master")
     dotnet nuget push *.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
     ;;
