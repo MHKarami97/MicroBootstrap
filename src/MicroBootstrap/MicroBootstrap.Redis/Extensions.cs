40b6adcf782117d5498305d20d7c06f1a1030602
mehdi hadeli <mehdi.hadeli@outlook.com>
2020-12-21 17:49:16 +0330

    .

--- src/MicroBootstrap/MicroBootstrap.Redis/Extensions.cs
+++ src/MicroBootstrap/MicroBootstrap.Redis/Extensions.cs
@@ -1,5 +1,5 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace MicroBootstrap.Redis
 {
@@ -9,18 +9,21 @@ namespace MicroBootstrap.Redis
 
         public static IServiceCollection AddRedis(this IServiceCollection services, string sectionName = SectionName)
         {
-             if (string.IsNullOrWhiteSpace(sectionName)) sectionName = SectionName;
+            if (string.IsNullOrWhiteSpace(sectionName)) sectionName = SectionName;
 
-             var options = services.GetOptions<RedisOptions>(sectionName);
+            var options = services.GetOptions<RedisOptions>(sectionName);
             return services.AddRedis(options);
         }
         public static IServiceCollection AddRedis(this IServiceCollection services, RedisOptions options)
         {
-            services.AddStackExchangeRedisCache(o =>
-            {
-                o.Configuration = options.ConnectionString;
-                o.InstanceName = options.Instance;
-            });
+            services.AddSingleton(options)
+           .AddSingleton<IConnectionMultiplexer>(sp => ConnectionMultiplexer.Connect(options.ConnectionString))
+           .AddTransient(sp => sp.GetRequiredService<IConnectionMultiplexer>().GetDatabase(options.Database))
+           .AddStackExchangeRedisCache(o =>
+           {
+               o.Configuration = options.ConnectionString;
+               o.InstanceName = options.Instance;
+           });
 
             return services;
         }
